1bec2a60f52dad33de07684d3a8c4081e6286033
A happy bear <142538281+brownbeardeveloper@users.noreply.github.com>
2023-11-26 14:33:55 +0100

    Update firebase-config.js

--- src/assets/firebase-config.js
+++ src/assets/firebase-config.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyArUSdjZOqgrTh7W6Yn2mi17H6HdxW_M50",
-  authDomain: "ddf-website-project.firebaseapp.com",
-  projectId: "ddf-website-project",
-  storageBucket: "ddf-website-project.appspot.com",
-  messagingSenderId: "347273855961",
-  appId: "1:347273855961:web:3969f8213b57366545cfc8",
-  measurementId: "G-SVW4RCZLG8"
+  apiKey: "?",
+  authDomain: "?",
+  projectId: "?",
+  storageBucket: "?",
+  messagingSenderId: "?",
+  appId: "?",
+  measurementId: "?"
 };
 
 // Initialize Firebase
